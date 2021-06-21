a9442791e4ba6f0fb488ab2706db16981a7b9313
nuonuo <2472500609@qq.com>
2021-06-22 02:06:22 +0800

    add some specifics

---
+++ src/main/java/com/spring/beansfactory/support/BeanSupportCandidate.java
@@ -0,0 +1,28 @@
+package com.spring.beansfactory.support;
+
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+
+public class BeanSupportCandidate {
+    final private ArrayList<Constructor<?>> candidates;
+    final private int size;
+    final private Constructor<?> noParamCon;
+
+    protected BeanSupportCandidate(ArrayList candidates,int size,Constructor noParamCon){
+        this.candidates = candidates;
+        this.size = size;
+        this.noParamCon = noParamCon;
+    }
+
+    public ArrayList getCandidates(){
+        return candidates;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public Constructor<?> getNoParamCon(){
+        return noParamCon;
+    }
+}
